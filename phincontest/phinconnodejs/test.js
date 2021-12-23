20841e292f56dbff9c68eaa59f5346569a0dd934
sandi permana <sandipermanasoebagio@gmail.com>
2021-12-23 10:32:37 +0700

    node js has been get the api and return it

--- phincontest/phinconnodejs/test.js
+++ phincontest/phinconnodejs/test.js
@@ -1,18 +1,30 @@
 const https = require('https');
 
 https.get('https://pokeapi.co/api/v2/pokedex/kanto/', (resp) => {
-  let data = '';
+    let data = '';
 
-  // A chunk of data has been received.
-  resp.on('data', (chunk) => {
+    // A chunk of data has been received.
+    resp.on('data', (chunk) => {
     data += chunk;
-  });
+    });
 
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
     console.log(JSON.parse(data));
-  });
+    });
+
+    const express = require('express')
+    const app = express()
+    const port = 3000
+
+    app.get('/', (req, res) => {
+    res.send(JSON.parse(data))
+    })
+
+    app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+    })
 
 }).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
+    console.log("Error: " + err.message);
+  });
